879cc40f902683ff7a5e9a8c9628e56b1d2d7bd6
heyy-rin <yatta0831@gmail.com>
2018-05-30 22:33:52 +0900

    이미지 출력 해결 실시간 기능 추가

--- client.py
+++ client.py
@@ -15,8 +15,23 @@
 import socket
 import cv2
 import numpy
+import time
 
-#연결할 서버(수신단)의 ip주소와 port번호
+def openCamera() :
+
+    #OpenCV를 이용해서 webcam으로 부터 이미지 추출
+    capture = cv2.VideoCapture(0)
+
+    if (capture.isOpened()):
+      print("cap is open")
+    else:
+      print("cap error")
+
+    return capture
+
+
+
+# #연결할 서버(수신단)의 ip주소와 port번호
 TCP_IP = '172.30.1.1'
 TCP_PORT = 5001
 
@@ -24,31 +39,39 @@ TCP_PORT = 5001
 sock = socket.socket()
 sock.connect((TCP_IP, TCP_PORT))
 
-#OpenCV를 이용해서 webcam으로 부터 이미지 추출
-capture = cv2.VideoCapture(0)
-if (capture.isOpened()):
-    print("cap is open")
-else:
-    print("cap error")
+capture = openCamera()
+while True :
+    ret, frame = capture.read()
 
-ret, frame = capture.read()
-cv2.imshow("FIRST", frame) #결과가 왜 까만가!
+    # 추출한 이미지를 String 형태로 변환(인코딩)시키는 과정
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+    data = numpy.array(imgencode)
+    stringData = data.tostring()
+
+    # 추출한 이미지를 String 형태로 변환(인코딩)시키는 과정
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+    data = numpy.array(imgencode)
+    stringData = data.tostring()
+
+    print("stringData :", stringData)
+
+    sock.send( str(len(stringData)).ljust(16).encode());
+    sock.send( stringData);
+    time.sleep(0.1)
+
+    decimg=cv2.imdecode(data,1)
+    cv2.imshow('CLIENT',decimg)
+
+    k = cv2.waitKey(1) & 0xff #stop & save
+    if (k == ord('s')):
+        cv2.imwrite('sample.png', frame)
+    elif (k == ord('q')):
+        break
+    #cv2.destroyAllWindows()
 
 #cv2.imwrite('sample.png', frame)
 
-#추출한 이미지를 String 형태로 변환(인코딩)시키는 과정
-encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-data = numpy.array(imgencode)
-stringData = data.tostring()
-
-#String 형태로 변환한 이미지를 socket을 통해서 전송
-sock.send( str(len(stringData)).ljust(16).encode());
-sock.send( stringData);
-sock.close()
-
-#다시 이미지로 디코딩해서 화면에 출력. 그리고 종료
-decimg=cv2.imdecode(data,1)
-cv2.imshow('CLIENT',decimg)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
+sock.close()
