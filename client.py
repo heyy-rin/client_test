cc25d907736100b4025f89318d0fc23d3c661f4d
heyy-rin <yatta0831@gmail.com>
2018-06-08 15:41:45 +0900

    사람 검출 기능 추가 검출된 이미지만 전송 가능

--- client.py
+++ client.py
@@ -16,6 +16,11 @@ import socket
 import cv2
 import numpy
 import time
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
 
 # return : camera 객체
 # 카메라가 정상적으로 작동하는지 확인함
@@ -60,11 +65,33 @@ def sendImage(frame) :
 
     return data;
 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# initialize the FPS counter
+fps = FPS().start()
 
 #연결할 서버(수신단)의 ip주소와 port번호
 #TCP_IP = '172.30.1.1'
 TCP_IP = '10.10.24.117'
-
 TCP_PORT = 5001
 
 #송신을 위한 socket 준비
@@ -76,16 +103,46 @@ while True :
     ret, frame = capture.read()
 
     #
+    frame = imutils.resize(frame, width=600)  # window size
 
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (600, 600)), 0.007843, (300, 300), 127.5)
 
-    #
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
 
-    # 인코딩하기 전에 사람 검출한 뒤 ! 해당되는 이미지만 보내기!
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
 
-    # 전송된 이미지가 원하는 결과였는지 확인하기 위해 client에서 출력
-    data = sendImage(frame) # 전송된 이미지배열이 저장됨!
-    decimg=cv2.imdecode(data,1) # 이미지 배열을 decode
-    cv2.imshow('CLIENT',decimg) # decode된 이미지를 확인해서 원하는 이미지가 전송되었는지 확인
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > args["confidence"]:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # draw the prediction on the frame
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            if (CLASSES[idx] == "person"): # 인코딩하기 전에 사람 검출한 뒤 ! 해당되는 이미지만 보내기!
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+                # 전송된 이미지가 원하는 결과였는지 확인하기 위해 client에서 출력
+                data = sendImage(frame)  # 전송된 이미지배열이 저장됨!
+                decimg = cv2.imdecode(data, 1)  # 이미지 배열을 decode
+                cv2.imshow('CLIENT', decimg)  # decode된 이미지를 확인해서 원하는 이미지가 전송되었는지 확인
 
     # cv2가 저장 또는 중지를 위해 키보드 입력을 기다림
     k = cv2.waitKey(1) & 0xff # stop & save
@@ -93,9 +150,18 @@ while True :
         cv2.imwrite('sample.png', frame) # save기능은 단순 확인용
     elif (k == ord('q')):
         break
+
+    # update the FPS counter
+    fps.update()
     #cv2.destroyAllWindows()
 
 #cv2.imwrite('sample.png', frame)
 
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# window 와 socket 을 닫음
 cv2.destroyAllWindows()
 sock.close()
